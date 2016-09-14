220a34f00234ef5bdad1596766133e9dbb3b5def
winter <artlenk.ru@gmail.com>
2016-09-14 23:43:50 +0300

    Fully impose blog

--- src/js/main.js
+++ src/js/main.js
@@ -1,54 +1,17 @@
-// Актальный год в подвале сайта
-var dateMy = new Date();
-var year = dateMy.getFullYear();
-document.getElementById('year').innerHTML = year;
-
-// Tooltip подсказка от bootstrap
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
+$(".menu-toggle").click(function () {
+    $(this).next().toggleClass("menu-open");
 });
-
-// Кнопка вверх
+/*
 $(function () {
+    var fixblock_height = $('.top-panel').height();
+    var fixblock_pos = $('.top-panel').position().top;
     $(window).scroll(function () {
-        if ($(this).scrollTop() != 0) {
-            $('#toTop').fadeIn();
+
+        if ($(window).scrollTop() > fixblock_pos) {
+            $('.top-panel').addClass('down-scroll');
         } else {
-            $('#toTop').fadeOut();
+            $('.top-panel').removeClass('down-scroll');
         }
-    });
-    $('#toTop').click(function () {
-        $('body,html').animate({scrollTop: 0}, 800);
-    });
+    })
 });
-
-$(document).ready(function () {
-
-    var prod = $("#producers");
-
-    prod.owlCarousel({
-        items: 5,
-        itemsDesktop: [1000, 5],
-        itemsDesktopSmall: [900, 3],
-        itemsTablet: [600, 2],
-        itemsMobile: false
-    });
-    $(".next").click(function () {
-        prod.trigger('owl.next');
-    });
-    $(".prev").click(function () {
-        prod.trigger('owl.prev');
-    });
-
-    //Для мобильного меню
-    $(".menu-toggle").click(function () {
-        $('.menu').toggleClass("open-menu");
-    });
-
-    //Плавная прокрутка к якорям на странице
-    $("body").on('click', '[href*="#"]', function (e) {
-        var fixed_offset = 100;
-        $('html,body').stop().animate({scrollTop: $(this.hash).offset().top - fixed_offset}, 1000);
-        e.preventDefault();
-    });
-});
+*/
